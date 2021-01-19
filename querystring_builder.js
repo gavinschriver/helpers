32ed5311c4b77f4171fc3cb6d99efaf4f5c05690
Gavin Schriver <alexanderschriver@gmail.com>
2021-01-18 20:16:32 -0600

    adding comments

--- querystring_builder.js
+++ querystring_builder.js
@@ -1,5 +1,15 @@
 /** return a string built from passing in an object
- * @param {Object} filters Object with k/v pairs of a relevant querystring param name and matching value; will only add the pair if the 'value' is not 0
+ * prepended with ? ; adds <key>=<value> for each kv pair in object; separates kvs in string with "&" character, omitted after last kv pair
+ *   
+ * @param {Object} filters Object with key/value pairs of a (k) relevant querystring param name and (v) matching value
+ * Will only add the pair if the 'value' is not 0
+ * 
+ * example:
+ * 
+ * buildQueryString({name: "gavo", year: 2000, dontAddThis: 0})
+ * // returns '?name=gavo&year=2000'
+ * 
+ * 
  */
 export const buildQueryString = (filters) => {
     const filtersArray = Object.entries(filters);
